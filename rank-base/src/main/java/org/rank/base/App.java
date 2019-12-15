e731972f236f0d78807b1d5a4099856686321ee4
eleme <eleme@elemedeMacBook-Pro.local>
2019-12-15 17:46:14 +0800

    add

---
+++ rank-base/src/main/java/org/rank/base/App.java
@@ -0,0 +1,13 @@
+package org.rank.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
